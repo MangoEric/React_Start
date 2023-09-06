fff1c4a74adab6fa28b646b2472391bbeb648186
Eric-JongYoung <fgh1937@yahoo.com>
2023-09-06 23:57:28 +0900

    [13] - img

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar, Container } from 'react-bootstrap';
+import 대문 from './bg.png';
 
 function App() {
   return (
@@ -10,12 +11,34 @@ function App() {
           <Navbar.Brand href="#home">Eric's Shop🌲</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <br />
+      <div className="main-bg" style={ { backgroundImage : 'url('+ 대문 +')' } }></div>
+      <div>
+        <img src={process.env.PUBLIC_URL + '/logo192.png'} id="logo"/>
+        Welcome To Eric's Shop
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" className='pic'/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" className='pic'/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" className='pic'/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div> 
     </div>
   );
 }
