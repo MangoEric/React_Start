952ade70c638f6b7e7069acc90a18fc94e1d95f6
MangoEric <fgh1937@yahoo.com>
2023-09-11 00:28:41 +0900

    [20] - LifeCycle & useEffect (2)

--- shop/src/routes/Detatil.js
+++ shop/src/routes/Detatil.js
@@ -2,23 +2,40 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 
-
 function Deatail(props) {
 
   useEffect(()=>{
-    // mount, update 시 코드를 실행해주는 useEffect
-    // html 랜더링 후에 동작합니다.
-    // console.log('안뇽')
-    // 어려운 연산
-    // for(var i = 0 ; i < 10000 ; i ++){
-    //   console.log(i);
-    // }
-    // 서버에서 데이터가져오는 작업 등등..
+    let a = setTimeout(()=>{ setAlert(false)}, 2000)
+    console.log(2)
+    // useEffect 가 실행되기 전에 실행됨 -> 별명 clean up function
+    return () => {
+      console.log(1)
+      //기존 타이머는 제거해주세요.
+      clearTimeout(a)
+      //기존 데이터요청은 제거해주세요.. 등등..
+    }
+  }, []) // 컴포넌트 mount 시 1회만 실행하고 싶으면 이렇게
 
-    setTimeout(()=>{
-      setAlert(false)
-    }, 2000)
-  })
+  // 1. 이러면 재렌더링마다 코드를 실행가능합니다.
+  // useEffect(()=>{ 실행할코드 })
+  // 2. 이러면 컴포넌트 mount시 (로드시) 1회만 실행가능합니다.
+  // useEffect(()=>{ 실행할코드 }, [])
+  // 3. 이러면 useEffect 안의 코드 실행 전에 항상 실행됩니다. 
+  // useEffect(()=>{ 
+  //   return ()=>{
+  //     실행할코드
+  //   }
+  // })
+  // 4. 이러면 컴포넌트 unmount시 1회 실행됩니다.
+  // useEffect(()=>{ 
+  //   return ()=>{
+  //     실행할코드
+  //   }
+  // }, [])
+  // 5. 이러면 state1이 변경될 때만 실행됩니다.
+  // useEffect(()=>{ 
+  //   실행할코드
+  // }, [state1])
 
   let [count, setCount] = useState(0);
   let [alert, setAlert] = useState(true);
@@ -46,10 +63,6 @@ function Deatail(props) {
           <p>{item.content}</p>
           <p>{item.price}</p>
           <button className="btn btn-danger">주문하기</button>
-        </div>
-        <div>
-          {count}
-          <button onClick={()=>{setCount(count+1)}}>버튼</button>
         </div>
       </div>
     </div>
